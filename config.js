40b48b64c540b689513556d1d80300d498b20b3e
npool.pw <36919430+npoolpw@users.noreply.github.com>
2018-06-05 15:04:01 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+var api = "https://api.npool.pw/pstar";
+
+var poolHost = "ca.npool.pw";
+
+var blockExplorer = "http://pstar.poolmining.us/#blockchain_blocks";
+
+var cryptonatorWidget = ["BTC-USD"];
+
+var blockchainExplorer = "http://pool.pinkstarcoin.com/?hash={id}#blockchain_block";
+
+var transactionExplorer = "http://pool.pinkstarcoin.com/?hash={id}#blockchain_transaction";
+
+var themeCss = "themes/default-theme.css";
+
+var networkStat = [
+        ["npool.pw/pstar", "https://api.npool.pw/pstar"],
+        ["pool.pinkstarcoin.com", "http://pool.pinkstarcoin.com:7111"]
+];
